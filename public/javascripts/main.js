6286aece6ebdf914ad4cd557e0b8a6845748a84b
thibautbessone <thibaut13b@gmail.com>
2019-06-24 18:17:47 +0200

    Added option fields & updated title

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,5 +3,23 @@ $(document).ready(function(){
 });
 
 $('#randomSelect').change(function(){
-    alert($("#randomSelect").val());
+    if($("#randomSelect").val() === 'rw') { // Weighted random
+        alert('its weighted random');
+    } else {
+        alert('its list random');
+    }
 });
+
+// Will have to add :
+/*
+<div class="row owPair">
+  <div class="col s4">
+    <input class="option" placeholder="Option" type="text" />
+  </div>
+  <div class="col s4">
+    <input class="weight" placeholder="Weight" type="text" />
+  </div>
+  <a class="waves-effect waves-light btn red">Remove</a>
+</div>
+ */
+// to add options
