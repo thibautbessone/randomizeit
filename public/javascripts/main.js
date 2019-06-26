1e0f9ba12b4a452d00c0d38034b5f7b6b1063239
thibautbessone <thibaut13b@gmail.com>
2019-06-26 11:34:36 +0200

    Create weighted random route, changed CSS & added dynamic input creation

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,25 +1,77 @@
-$(document).ready(function(){
+const animDelay = 100; // ms
+
+const newOptionHtml =
+    '<div class="row owPair">\n' +
+    '  <div class="col s4">\n' +
+    '    <input class="option" placeholder="Option" type="text" />\n' +
+    '  </div>\n' +
+    '  <div class="col s4">\n' +
+    '    <input class="weight" placeholder="Weight" type="text" />\n' +
+    '  </div>\n' +
+    '  <div class="col s4">\n' +
+    '  <a class="waves-effect waves-light btn red removeBtn">Remove</a>\n' +
+    '  </div>\n' +
+    '</div>';
+
+const newEntryHtml =
+    '<div class="row listEntry">\n' +
+    '  <div class="col s8">\n' +
+    '    <input class="option" placeholder="Option" type="text" />\n' +
+    '  </div>\n' +
+    '  <div class="col s4">\n' +
+    '  <a class="waves-effect waves-light btn red removeBtn">Remove</a>\n' +
+    '  </div>\n' +
+    '</div>';
+
+$(document).ready(function() { // Materialize.css select
     $('select').formSelect();
 });
 
-$('#randomSelect').change(function(){
+$('#randomSelect').change(function() { // I don't like callbacks
     if($("#randomSelect").val() === 'rw') { // Weighted random
-        alert('its weighted random');
+        $('#listRandom').fadeOut(animDelay, function () {
+            $('#weightedRandom').fadeIn(animDelay);
+        });
     } else {
-        alert('its list random');
+        $('#weightedRandom').fadeOut(animDelay, function () {
+            $('#listRandom').fadeIn(animDelay);
+        });
     }
 });
 
-// Will have to add :
-/*
-<div class="row owPair">
-  <div class="col s4">
-    <input class="option" placeholder="Option" type="text" />
-  </div>
-  <div class="col s4">
-    <input class="weight" placeholder="Weight" type="text" />
-  </div>
-  <a class="waves-effect waves-light btn red">Remove</a>
-</div>
- */
-// to add options
+
+$('.newOption').on('click', function () {
+    $(this).closest('.row').after(newOptionHtml);
+});
+
+$('.newEntry').on('click', function () {
+    $(this).closest('.row').after(newEntryHtml);
+});
+
+$('.row').on('click', '.removeBtn', function () {
+    $(this).closest('.row').remove();
+});
+
+$('.submitOWBtn').click(function () {
+    // Constructing JSON array dynamically yeet
+    let optionsString = '[';
+    $('.owPair').each(function () {
+        optionsString += '{"option": "' + $(this).find('.option').val() + '", "weight": ' + $(this).find('.weight').val() + '},';
+    });
+    optionsString = optionsString.slice(0, -1) + ']';
+    $.ajax({
+        traditional: true,
+        type: 'POST',
+        url: window.location.pathname + 'random/weighted',
+        data: {
+            options: optionsString
+        },
+        success: function (msg) {
+            alert(msg);
+        }
+    });
+});
+
+$('.submitListBtn').click(function () {
+    alert('lol');
+});
--- routes/random.js
+++ routes/random.js
@@ -1,10 +1,18 @@
-const wariai = require('wariai');
+const Picker = require('wariai');
 
 const express = require('express');
 const router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/weighted', function(req, res, next) {
+  console.log((req.body.options));
+  let picker  = new Picker(req.body.options);
+  res.send(JSON.stringify(picker.pickOneWithWeight()));
 });
 
+
+process.on('uncaughtException', function (exception) {
+  console.log(exception); // to see your exception details in the console
+  // if you are on production, maybe you can send the exception details to your
+  // email as well ?
+});
 module.exports = router;
